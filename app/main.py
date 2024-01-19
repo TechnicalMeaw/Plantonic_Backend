6729dd040519ac5686bfe3eb0487b5e7db3907e2
technicalmeaw <technicalmeaw@gmail.com>
2024-01-20 00:28:58 +0530

    webpage and static files added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from .database import engine
-from .routers import auth, delivery, homepage, profilepage
+from .routers import auth, delivery, homepage, profilepage, webpage
 from .blue_dart import api
 
 
@@ -8,6 +9,10 @@ from .blue_dart import api
 
 app = FastAPI()
 
+# Static files
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+app.include_router(webpage.router)
 app.include_router(auth.router)
 app.include_router(delivery.router)
 app.include_router(homepage.router)
---
+++ app/routers/webpage.py
@@ -0,0 +1,21 @@
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import APIRouter
+
+
+router = APIRouter()
+
+templates = Jinja2Templates(directory="app/templates")
+
+@router.get("/", response_class=HTMLResponse)
+async def intro(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@router.get("/privacy-policy", response_class=HTMLResponse)
+async def intro(request: Request):
+    return templates.TemplateResponse("privacypolicy.html", {"request": request})
+
+@router.get("/terms-and-condition", response_class=HTMLResponse)
+async def intro(request: Request):
+    return templates.TemplateResponse("termcondition.html", {"request": request})
