5ae8b8c53a1029d9d030963ddc3a7f8f64b28be3
technicalmeaw <technicalmeaw@gmail.com>
2024-01-27 18:52:34 +0530

    server debugging

--- app/routers/delivery.py
+++ app/routers/delivery.py
@@ -39,105 +39,107 @@ def check_pin_code(pincode: str, current_user : models.User = Depends(oauth2.get
 
 @router.post("/place_order", response_model=schemas.PaceOrderResponseModel)
 def place_order(all_orders: schemas.PlaceOrderRequestModel, db: Session = Depends(get_db), current_user : models.User = Depends(oauth2.get_current_user)):
-    try:
-        for order in all_orders.all_orders:
-            # product info
-            product = realtime_db.get_product_info(order.productId)
-            product_price = math.ceil(eval(product['actualPrice']))
-            product_listed_price = product['listedPrice']
+    # try:
+    for order in all_orders.all_orders:
+        # product info
+        product = realtime_db.get_product_info(order.productId)
+        product_price = math.ceil(eval(product['actualPrice']))
+        product_listed_price = product['listedPrice']
 
-            # customer info
-            customer_name = order.customerName
-            customer_phone = order.phoneNo
-            customer_email = order.email
-            customer_address = order.address
-            pincode = order.pincode
+        # customer info
+        customer_name = order.customerName
+        customer_phone = order.phoneNo
+        customer_email = order.email
+        customer_address = order.address
+        pincode = order.pincode
 
-            # money
-            quantity = order.quantity
-            payable = product_price
-            delivery_charge = eval(order.deliveryCharge)
+        # money
+        quantity = order.quantity
+        payable = product_price
+        delivery_charge = eval(order.deliveryCharge)
 
-            if delivery_charge > 0:
-                payable += delivery_charge
+        if delivery_charge > 0:
+            payable += delivery_charge
+        
+        for _ in range(quantity):
+            bd_response = bd.generate_waybill(full_address=customer_address, 
+                            email= customer_email if customer_email is not None else "", 
+                            phone_number= customer_phone,
+                            name = customer_name,
+                            pincode = pincode,
+                            is_reverse_pickup = False,
+                            product_name = product['productName'],
+                            product_id = order.productId,
+                            special_instruction = order.special_instructions,
+                            product_count = 1, # Need special calculations
+                            order_id= order.orderId,
+                            pickup_date= utils.get_pickup_date(),
+                            pickup_time= '1600',
+                            breath= float(product['breadth']),
+                            height= float(product['height']),
+                            length= float(product['length']),
+                            weight= product['weight'],
+                            box_count= 1, # Default
+                            amount_collectable= payable,
+                            register_for_pickup= settings.is_live # Need to change when production
+                            )
+        
             
-            for _ in range(quantity):
-                bd_response = bd.generate_waybill(full_address=customer_address, 
-                                email= customer_email if customer_email is not None else "", 
-                                phone_number= customer_phone,
-                                name = customer_name,
-                                pincode = pincode,
-                                is_reverse_pickup = False,
-                                product_name = product['productName'],
-                                product_id = order.productId,
-                                special_instruction = order.special_instructions,
-                                product_count = 1, # Need special calculations
-                                order_id= order.orderId,
-                                pickup_date= utils.get_pickup_date(),
-                                pickup_time= '1600',
-                                breath= float(product['breadth']),
-                                height= float(product['height']),
-                                length= float(product['length']),
-                                weight= product['weight'],
-                                box_count= 1, # Default
-                                amount_collectable= payable,
-                                register_for_pickup= settings.is_live # Need to change when production
+            if not bd_response or not bd_response['GenerateWayBillResult'] or not bd_response['GenerateWayBillResult']['Status']:
+                continue
+
+
+            bd_order = models.BlueDartOrders(bd_awb_no = bd_response['GenerateWayBillResult']['AWBNo'],
+                                bd_ccrcrdref = bd_response['GenerateWayBillResult']['CCRCRDREF'],
+                                bd_cluster_code = bd_response['GenerateWayBillResult']['ClusterCode'],
+                                bd_destination_area = bd_response['GenerateWayBillResult']['DestinationArea'],
+                                bd_destination_location = bd_response['GenerateWayBillResult']['DestinationLocation'],
+                                bd_is_error = bd_response['GenerateWayBillResult']['IsError'],
+                                bd_token_number = bd_response['GenerateWayBillResult']['TokenNumber'],
+                                bd_status_information = bd_response['GenerateWayBillResult']['Status'][0]['StatusInformation']
                                 )
             
-                
-                if not bd_response or not bd_response['GenerateWayBillResult'] or not bd_response['GenerateWayBillResult']['Status']:
-                    continue
+            # Save PDF
+            with open(f"./app/static/labels/{bd_response['GenerateWayBillResult']['AWBNo']}.pdf", 'wb') as f:
+                f.write(bytearray(bd_response['GenerateWayBillResult']['AWBPrintContent']))
 
-                # Save PDF
-                with open(f"./app/static/labels/{bd_response['GenerateWayBillResult']['AWBNo']}.pdf", 'wb') as f:
-                    f.write(bytearray(bd_response['GenerateWayBillResult']['AWBPrintContent']))
+            db.add(bd_order)
+            db.commit()
+            db.refresh(bd_order)
 
-                bd_order = models.BlueDartOrders(bd_awb_no = bd_response['GenerateWayBillResult']['AWBNo'],
-                                    bd_ccrcrdref = bd_response['GenerateWayBillResult']['CCRCRDREF'],
-                                    bd_cluster_code = bd_response['GenerateWayBillResult']['ClusterCode'],
-                                    bd_destination_area = bd_response['GenerateWayBillResult']['DestinationArea'],
-                                    bd_destination_location = bd_response['GenerateWayBillResult']['DestinationLocation'],
-                                    bd_is_error = bd_response['GenerateWayBillResult']['IsError'],
-                                    bd_token_number = bd_response['GenerateWayBillResult']['TokenNumber'],
-                                    bd_status_information = bd_response['GenerateWayBillResult']['Status'][0]['StatusInformation']
-                                    )
-                db.add(bd_order)
-                db.commit()
-                db.refresh(bd_order)
+            if bd_response['GenerateWayBillResult']['IsError']:
+                continue
 
-                if bd_response['GenerateWayBillResult']['IsError']:
-                    continue
+            new_order = models.Orders(customer_id= current_user.id,
+                        product_id= order.productId,
+                        merchant_id= order.merchantId,
+                        customer_name= customer_name,
+                        customer_full_address= customer_address,
+                        customer_pincode= pincode,
+                        customer_address_type= order.addressType,
+                        customer_phone_number= customer_phone,
+                        customer_payment_method= order.paymentMethod,
+                        actual_order_quantity= quantity,
+                        related_to_order_id= order.orderId,
+                        transaction_id= order.transactionId,
+                        payable= payable,
+                        delivery_charge = delivery_charge,
+                        product_listed_price= product_listed_price,
+                        bd_order_id= bd_order.bd_awb_no,
+                        special_instructions= order.special_instructions
+                        )
+            
+            db.add(new_order)
+            db.commit()
 
-                new_order = models.Orders(customer_id= current_user.id,
-                            product_id= order.productId,
-                            merchant_id= order.merchantId,
-                            customer_name= customer_name,
-                            customer_full_address= customer_address,
-                            customer_pincode= pincode,
-                            customer_address_type= order.addressType,
-                            customer_phone_number= customer_phone,
-                            customer_payment_method= order.paymentMethod,
-                            actual_order_quantity= quantity,
-                            related_to_order_id= order.orderId,
-                            transaction_id= order.transactionId,
-                            payable= payable,
-                            delivery_charge = delivery_charge,
-                            product_listed_price= product_listed_price,
-                            bd_order_id= bd_order.bd_awb_no,
-                            special_instructions= order.special_instructions
-                            )
-                
-                db.add(new_order)
-                db.commit()
+        # Delete product from cart
+        realtime_db.delete_product_from_cart(user_id=current_user.firebase_uid, product_id=order.productId)
 
-            # Delete product from cart
-            realtime_db.delete_product_from_cart(user_id=current_user.firebase_uid, product_id=order.productId)
+        # Update stocks
+        realtime_db.update_product_quantity(product_id=order.productId, quantity=order.quantity)
 
-            # Update stocks
-            realtime_db.update_product_quantity(product_id=order.productId, quantity=order.quantity)
-
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_424_FAILED_DEPENDENCY, detail="Something went wrong, some of the orders may not have been placed.")
+    # except Exception:
+    #     raise HTTPException(status_code=status.HTTP_424_FAILED_DEPENDENCY, detail="Something went wrong, some of the orders may not have been placed.")
     return {"status": "Valid", "detail": "Order placed successfully"}
 
 
