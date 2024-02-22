a35548e78e7bffa608996c55697f8d54bd814c93
technicalmeaw <technicalmeaw@gmail.com>
2024-02-22 20:21:57 +0530

    middleware added - CORS error fixed

--- app/main.py
+++ app/main.py
@@ -2,12 +2,20 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from .database import engine
 from .routers import auth, delivery, homepage, profilepage, webpage
-from .blue_dart import api
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Static files
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
